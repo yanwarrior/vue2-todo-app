5438cd1d3f894204005dd6a8395d84ff5b3e6a2f
Yanwar Solahudin <yanwar@staff.gramedia.com>
2018-04-14 03:16:16 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# vueapp1
+Learning vue 2 (using vue-cli) basic crud todo app.
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/components/TodoList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <span>Completed Tasks: {{ todos.filter(todo => {return todo.done === true}).length }}</span>
+    <br>
+    <span>Pending Tasks: {{ todos.filter(todo => {return todo.done === false}).length }}</span>
+    <todo v-on:delete-todo="deleteTodo" v-on:complete-todo="completeTodo" v-for="todo in todos" :key="todo.id" v-bind:todo="todo"></todo>
+	</div>
+</template>
+
+<script>
+  import Todo from './Todo';
+
+  export default {
+    props: ['todos'],
+    components: {
+      Todo,
+    },
+    methods: {
+      deleteTodo(todo) {
+        const todoIndex = this.todos.indexOf(todo);
+        this.todos.splice(todoIndex, 1);
+      },
+      completeTodo(todo) {
+        const todoIndex = this.todos.indexOf(todo);
+        this.todos[todoIndex].done = true;
+      }
+    },
+  }
+</script>
+
+<style>
+
+</style>
